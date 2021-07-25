897283b93c88f015c2fed80540633a417966eb8e
DroidZed <aymen.dragon5@gmail.com>
2021-07-25 11:01:32 +0100

    initial setup

---
+++ src/main/java/me/droidzed/springmailerapp/SpringMailerAppApplication.java
@@ -0,0 +1,13 @@
+package me.droidzed.springmailerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMailerAppApplication.class, args);
+    }
+
+}
